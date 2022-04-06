ac76aa82cf7293a665b18373fa3ffbd68253cef6
andrew <andrew19@live.fr>
2022-04-06 13:06:42 +0200

    ok

--- inc/Commands/Command.hpp
+++ inc/Commands/Command.hpp
@@ -1,9 +1,9 @@
 #pragma once
-
 #include "includes.h"
 #include "../Server.hpp"
 #include "../Client.hpp"
 
+class Server;
 class Command
 {
     protected:
--- inc/includes.h
+++ inc/includes.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <iostream>
-#include <string>
+#include <string> 
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/types.h>
