6d9bca23790d3559ac17312803862a15ae79c02e
Andrew Colin <acolin@c2r10p2.42nice.fr>
2022-05-03 13:34:00 +0200

    kick

--- srcs/Commands/CommandKick.cpp
+++ srcs/Commands/CommandKick.cpp
@@ -12,6 +12,11 @@ int CommandQuit::exec(Client *c)
 {
     if (this->argv.size() != 2)
         c->sendMessage(ERR_NEEDMOREPARAMS, "Not enough parameters");
-    
+    if (1)//!exist(argv[0]))
+        c->sendMessage(ERR_NOSUCHCHANNEL, argv[0] + ":No such channel");
+    if (1)//c->isop()
+        c->sendMessage(ERR_CHANOPRIVSNEEDED, "You're not channel operator");
+    if (1) //!isInChan(c->getUsername()) <nick> <channel> :They aren't on that channel
+        c->sendMessage(ERR_USERNOTINCHANNEL, c->getNickname() + " " + argv[1] + " :They aren't on that channel")
     return 0;
 }
