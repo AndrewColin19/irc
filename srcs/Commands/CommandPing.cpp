eac7d7de13c1e5a0c9d36244f95f184ecb25ec74
Loan Matarise <lmataris@c2r6p3.42nice.fr>
2022-05-05 15:13:04 +0200

    fsfds

--- srcs/Commands/CommandPing.cpp
+++ srcs/Commands/CommandPing.cpp
@@ -12,6 +12,5 @@ int CommandPing::exec(Client *c)
 {
     if (this->argv.size() == 0)
         return !c->sendMessage(ERR_NEEDMOREPARAMS, "Not enough parameters");
-    cout << "PONG" << endl;
-    return c->sendRawMessage("PONG ft_irc");
+    return c->sendRawMessage(":ft_irc PONG ft_irc");
 }
